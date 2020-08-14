cb6023f8b30def7d78ad6e441c96a1b86897e3ce
Luko <luko.web@gmail.com>
2020-08-14 20:26:26 -0300

    Add Playground Clase 4

---
+++ Xcode/Xcode/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Xcode
+//
+//  Created by Luciano Schillagi on 8/13/20.
+//  Copyright © 2020 Luko. All rights reserved.
+//
+
+import SwiftUI
+
+// MARK: Marca Uno
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+// MARK: Marca Dos
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- playgrounds/Clase_4.playground/Contents.swift
+++ playgrounds/Clase_4.playground/Contents.swift
@@ -1,3 +1,58 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+/*
+ CLASE 4
+ Topicos:
+ 
+ 1.Operadores de comparación
+ 
+ 2.Strings & Characters (2da parte)
+    -Contando caracteres
+    -Accediendo y modificando un string
+    -Substrings
+    -Comparando strings
+    -Representaciones Unicode de strings
+ */
+
+/// Operadores de comparación
+
+/*
+ Equal to (a == b)
+ Not equal to (a != b)
+ Greater than (a > b)
+ Less than (a < b)
+ Greater than or equal to (a >= b)
+ Less than or equal to (a <= b)
+ */
+
+// Ejemplos
+ 1 == 2  // true because 1 is equal to 1
+ 2 != 1   // true because 2 is not equal to 1
+ 2 > 1    // true because 2 is greater than 1
+ 1 < 2    // true because 1 is less than 2
+ 1 >= 1   // true because 1 is greater than or equal to 1
+ 2 <= 1   // false because 2 is not less than or equal to 1
+
+// Ejercicios:
+// 1: Comparar dos Ints y evaluar si contienen el mismo valor
+// ✍️
+// 2: Comparar dos Strings y evaluar si contienen el mismo valor
+// ✍️
+// 3: Comparar dos Doubles y evaluar si contienen valores distintos
+// ✍️
+// 4: Comparar dos Floats y evaluar si uno es mayor que el otro
+// ✍️
+// 5: Comparar dos Ints y evaluar si uno es menor o igual que el otro
+// ✍️
+
+/// Strings & Characters (2da parte)
+
+// Contando caracteres
+let saludo: String = "Hola Mundo"
+print(saludo.count)
+
+// 6: Declarar tres constantes que almacenen strings literales. Luego contar sus caracteres e imprimirlos en consola.
+// ✍️
+
+// 7: Obtener el primer caracter de las tres constantes anteriores. Imprimirlos.
+// ✍️
