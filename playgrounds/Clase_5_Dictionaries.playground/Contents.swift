5e786dbace4ed4c33c5c60a6d325cdbf3806b269
Luko <luko.web@gmail.com>
2020-08-19 19:38:58 -0300

    Add Sets and Dictionaries Playgrounds

---
+++ playgrounds/Clase_5_Dictionaries.playground/Contents.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+/*
+CLASE 5
+Tópicos:
+
+-Colecciones (Collections)
+-For in Loops (Bucles)
+*/
+
+/// Colecciones
+/// 3/3 Diccionarios
+/// Un Diccionario almacena asociaciones entre claves y valores en una colección sin un orden definido. Cada valor está asociado con una clave única que actúa como un identificador para ese valor. A diferencia de un array, los elementos de un diccionario no tienen un orden específico. Utiliza este tipo de colección cuando necesites asociarle a determinados valores una clave (o identificador) que describa qué representan.
+
+// Coleccion Type: Dictionary
+
+/* Como se escribe-declara un Dictionary en Swift */
+
+// De esta forma se escribe la sintáxis completa para declarar un diccionario
+
+// Dictionary<Key, Value>
+
+// donde "Key" es la clave asociada al valor y "Value" el valor en sí mismo
+// NOTA: los Keys y los Values de un diccionario deben ser de un mismo tipo
+// Ej: Dictionary <String, String>
+
+// EJERCICIO 01:
+// Declarar un diccionario vacío con la sintáxis de inicialización
+// ✍️
+// Agregarle tres elementos
+// ✍️
+// Actualizar un elemento (cambiar su valor anterior a uno nuevo)
+// ✍️
+// Remover un elemento del diccionario
+// ✍️
+// Evaluar si el diccionario está vacío
+// ✍️
+// Contar cuantos elementos tiene
+// ✍️
+// Recorrer el diccionario e imprimir la clave y el valor de cada uno de sus elementos
+// ✍️
+
+/* ********************************************************************** */
+// Desafío 🕵️‍♂️
+// Investigar cómo podríamos ordenar un diccionario
+// Existe un método para esto?
+// Dar un ejemplo
+// ✍️
+/* ********************************************************************** */
+
